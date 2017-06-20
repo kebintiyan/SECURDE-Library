0ba35d20a3e60ee1605f425b9d0c04f1b41a30b7
Kevin Chan <kevin98gray@gmail.com>
2017-06-20 17:29:11 +0800

    Initial commit

---
+++ src/main/java/com/securde/SecurdeApplication.java
@@ -0,0 +1,12 @@
+package com.securde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurdeApplication.class, args);
+	}
+}
