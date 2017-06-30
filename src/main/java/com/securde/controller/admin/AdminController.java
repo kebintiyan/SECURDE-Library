9236b48a0e20419d1d3c98d61e311bd8ff6258ab
Kevin Chan <kevin98gray@gmail.com>
2017-06-30 20:26:20 +0800

    Create admin

--- src/main/java/com/securde/controller/admin/AdminController.java
+++ src/main/java/com/securde/controller/admin/AdminController.java
@@ -1,5 +1,7 @@
 package com.securde.controller.admin;
 
+import com.securde.model.account.Role;
+import com.securde.model.account.User;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +13,27 @@ import org.springframework.web.servlet.ModelAndView;
  */
 
 @Controller
-public class AdminViewController {
+public class AdminController {
 
     @RequestMapping(value = {"/admin/home"}, method = RequestMethod.GET)
-    public ModelAndView home(Authentication authentication) {
+    public ModelAndView home() {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("admin/home");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = {"/admin/create"}, method = RequestMethod.GET)
+    public ModelAndView viewCreate() {
+        ModelAndView modelAndView = new ModelAndView();
+
+        User user = new User();
+        modelAndView.addObject("user", user);
+
+        String[] roles = {"Library Manager", "Library Staff"};
+        modelAndView.addObject("roles", roles);
+
+        modelAndView.setViewName("admin/create");
+
         return modelAndView;
     }
 }
